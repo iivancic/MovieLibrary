b8371e6356ef2a199937a74696e04ae445796f4d
Irena Ivančić <irena.ivancic7@gmail.com>
2020-01-07 23:14:30 +0100

    Erased HomePage default styling.

--- MovieLibrary.Web/ClientApp/src/components/NavMenu.js
+++ MovieLibrary.Web/ClientApp/src/components/NavMenu.js
@@ -36,10 +36,7 @@ export class NavMenu extends Component {
                                     <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                                    <NavLink tag={Link} className="text-dark" to="/">Overview</NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink tag={Link} to="/myServer/admin/">Admin page</NavLink>
